96e41ba1afe0f5b4dba2b16aaf40758b4b0a5379
vitor <vitor@DESKTOP-4OIHHQ9>
2023-04-28 15:22:24 -0300

    Add project files.

---
+++ Membros estaticos 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+
+    static double Pi = 3.14;
+
+    private static void Main(string[] args)
+    {
+
+
+        Console.Write("Informe aqui o raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double circ = Circunferencia(raio);
+        double volume = Volume(raio);
+
+        Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Valor de pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+
+    static double Circunferencia(double r)
+    {
+        return 2.0 * Pi * r;
+    }
+
+    static double Volume(double r)
+    { 
+        return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+    }
+
+}
